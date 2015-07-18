ca66a8707bbd7f05a04714ef706d632121898b79
Jacob Foshee <jacobf@gmail.com>
2015-07-18 11:53:17 -0500

    ServiceStack and testing dependencies

---
+++ ServiceStack.Authentication.Aad.Tests/AadAuthProviderTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ServiceStack.Auth;
+
+namespace ServiceStack.Authentication.Aad.Tests
+{
+    public class AadAuthProviderTest
+    {
+        [Test]
+        public void ShouldBeAuthProvider()
+        {
+            var subject = new AadAuthProvider();
+            subject.Should().BeAssignableTo<AuthProvider>();
+        }
+    }
+}
---
+++ ServiceStack.Authentication.Aad/AadAuthProvider.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStack.Auth;
+using System;
+
+namespace ServiceStack.Authentication.Aad
+{
+    public class AadAuthProvider : AuthProvider
+    {
+        public override bool IsAuthorized(IAuthSession session, IAuthTokens tokens, Authenticate request = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object Authenticate(IServiceBase authService, IAuthSession session, Authenticate request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
